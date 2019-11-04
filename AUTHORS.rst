0a12e788be9046c43ea7909f373f0ec13a2f8c21
Daffa Dewantara <55339155+daffa-dewantr@users.noreply.github.com>
2019-11-04 10:30:13 +0700

    Fix folder structure

--- AUTHORS.rst
+++
@@ -1,5 +0,0 @@
-============
-Contributors
-============
-
-* Daffa Dewantara <55339155+daffa-dewantr@users.noreply.github.com>
---
+++ src/trgauss_calc/main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import InterpolatePol as ip
+
+print("========================")
+print("Menu Utama")
+print("========================")
+print("1. Metode Gauss")
+print("1. Metode Trapezoidal")
+print("3. Metode Regresi Kuadrat Terkecil")
+print("4. Metode Interpolasi Polinom Newton")
+print("5. Tentang Program")
+print("------------------------")
+
+a = int(input("Masukan menu untuk melanjutkan: "))
+
+if a==1:
+    """Gauss
+    """    
+    print("========================")    
+    print("1. Sistem Persamaan Linier (SPL) 3 Variabel")
+    print("2. SPL 4 Variabel")
+    print("3. SPL 5 Variabel")
+    print("4. Kembali ke menu sebelumnya")
+    print("0. Kembali ke menu utama")
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+elif a==2:
+
+elif a==3:
+
+elif a==4:
+    
+elif a==5:
+    
+        
